0a27cbd9b38198ee5d2fae9e3e0eb482ca03424f
maimouna <164017136+nynasy@users.noreply.github.com>
2024-06-12 15:21:16 +0200

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -26,62 +26,70 @@ console.log("nombre de slides :  " + numberOfSlides);
 
 let divDots = document.getElementById("dots");
 
+// on crée les bullets point 
 for(let i = 0; i < numberOfSlides; i = i + 1) {
 	let spanDot = document.createElement("span");
 	spanDot.classList.add("dot");
-
-	if(i === currentIndex){
-		spanDot.classList.add("dot_selected");
-	}
-
 	divDots.appendChild(spanDot);
 }
 
+// on séléctionne le bullet point courant
+selectDot(currentIndex);
 
 buttonLeft.onclick = function () {
 	console.log("clique sur le bouton gauche");
-	//alert("clique sur le bouton gauche");
-	processElement(-1);
+	let nextIndex;
+	 //Si on est à la première image  
+	if(currentIndex === 0){
+		nextIndex = numberOfSlides - 1;
+	} 
+	// sinon on va sur l'element précédent 
+	else {
+		nextIndex = currentIndex - 1; 
+	}	
+	// on change de slide
+	changeSlide(nextIndex);
   };
 
 buttonRight.onclick = function () {
 	console.log("clique sur le bouton droit");
-	//alert("clique sur le bouton droit");
-	processElement(+1);
+	let nextIndex;
+	//Si on est à la dernière image 
+	if(currentIndex === numberOfSlides - 1){
+		nextIndex = 0;
+	} 
+	//sinon on va sur l'element suivant 
+	else {
+		nextIndex = currentIndex + 1; 
+	}	
+	// on change de slide
+	changeSlide(nextIndex);
 
   };
 
-function processElement(counter){
-	console.log("index courant : " + currentIndex);
 
-	//on change le bullet point actif au suivant 
+function changeSlide(nextIndex){
+	console.log("index courant : " + currentIndex);
+	console.log("index suivant : " + nextIndex);
 
 	// on désélectionne l'élément courant
-	divDots.children[currentIndex].classList.remove("dot_selected");
-
-	// on passe à l'élément suivant du tableau
-
-	//Si on est à la dernière image et que l’on clique à droite 
-	if(counter === +1 && currentIndex === numberOfSlides - 1){
-		currentIndex = 0;
-	} 
-	 //Si on est à la première image et qu’on clique à gauche 
-	else if(counter === -1 && currentIndex === 0){
-		currentIndex = numberOfSlides - 1;
-	} 
-	// on va sur l'element suivant ou précédent selon le counter
-	else {
-		currentIndex = currentIndex + counter; 
-	}	
+	divDots.children[currentIndex].classList.remove("dot_selected");	
 
 	// on séléctionne l'élément suivant
-	divDots.children[currentIndex].classList.add("dot_selected");
+	selectDot(nextIndex);
 	
 	//on change l’image 
 	let slideImage = document.getElementById("banner-img");
-	slideImage.setAttribute("src", "./assets/images/slideshow/" + slides[currentIndex]["image"]);
+	slideImage.setAttribute("src", "./assets/images/slideshow/" + slides[nextIndex]["image"]);
 
 	//on change le texte correspondant à l’image
 	let slideText = document.getElementById("banner-text");
-	slideText.innerHTML = slides[currentIndex]["tagLine"];
+	slideText.innerHTML = slides[nextIndex]["tagLine"];
+
+	// on remplace l'index courant par l'index suivant
+	currentIndex = nextIndex;
+}
+
+function selectDot(dotIndex){ 
+	divDots.children[dotIndex].classList.add("dot_selected");
 }
